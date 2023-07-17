df31fe30ec1b8ef6d82e208261b133ecc5fefbaa
cemcak <cem_cakmakci@hotmail.com>
2023-07-17 16:52:38 +0300

    #task_1001 IdentityServer added with configurations

---
+++ IdentityServer/GenericCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using GenericCourse.IdentityServer.DTOs;
+using GenericCourse.IdentityServer.Models;
+using GenericCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace GenericCourse.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDTO signupDTO)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDTO.UserName,
+                Email = signupDTO.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ IdentityServer/GenericCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,35 @@
+﻿using GenericCourse.IdentityServer.Models;
+using IdentityModel;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GenericCourse.IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var user = await _userManager.FindByEmailAsync(context.UserName);
+
+            if (user == null)
+                return;
+
+            var checkPassword = await _userManager.CheckPasswordAsync(user, context.Password);
+
+            if (!checkPassword)
+                return;
+
+            context.Result = new GrantValidationResult(user.Id.ToString(), OidcConstants.AuthenticationMethods.Password);
+
+        }
+    }
+}
